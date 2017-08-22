31e8ecae9c56815e32e8228a90f8476890bcb34b
Frans Peter Larsen <larsen.frans@gmail.com>
2017-08-22 20:52:38 +0200

    Working on AssetLoader, recognising extensions

--- Assets/Scripts/AssetsLoader.cs
+++ Assets/Scripts/AssetsLoader.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System;
 
 /*
  * Loading Model, Textures, Etc, into Unity
@@ -12,17 +14,63 @@ public class AssetsLoader : MonoBehaviour
 	private MeshRenderer renderer;
 	private Material material;
 
+    private List<Mesh> meshes;
+    private string applicationPath;
+
 	// Use this for initialization
 	void Start () 
 	{
 		filter = GetComponent<MeshFilter>();
 		renderer = GetComponent<MeshRenderer>();
 		material = renderer.material;
-	}
+
+        meshes = new List<Mesh>();
+        applicationPath = Application.dataPath;
+    }
 	
 	// Update is called once per frame
-	void Update () 
+	void FixedUpdate () 
 	{
-		
-	}
+        DirectoryInfo info = new DirectoryInfo(applicationPath);     // This should be in the root of the App (The dir where the EXE is found)
+        FileInfo[] fileInfo = info.GetFiles();                              
+
+        foreach(FileInfo file in fileInfo)
+        {
+            string extension = file.Extension;
+            bool isModel = false;
+
+            foreach(var ext in Enum.GetValues(typeof(ModelExtensions)))
+            {
+                isModel = String.Equals(extension, "." + ext.ToString(), StringComparison.CurrentCultureIgnoreCase);
+                if (isModel) break;
+            }
+
+            if(isModel)
+            {
+                
+            }
+        }
+    }
+}
+
+enum ModelExtensions
+{
+    OBJ,
+    FBX,
+    BLEND
+}
+
+enum ImageExtensions
+{
+    JPG,
+    PNG
+}
+
+//TODO Look for TextureMapTypes
+enum TextureMapTypes {
+    ALBEDO,
+    NORMAL,
+    HEIGHTMAP,
+    AO,
+    METALLIC
 }
